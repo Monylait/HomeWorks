448b50a9271d43debc31ea22f63c9caa793442e2
Monylait <48233779+Monylait@users.noreply.github.com>
2020-07-01 20:40:28 +1000

    Add files via upload

--- Lab_6.py
+++ Lab_6.py
@@ -1,6 +1,7 @@
 import os
 import multiprocessing as mp
 import csv
+import asyncio
 from tkinter import *
 from tkinter import messagebox
 from tkinter import filedialog
@@ -14,122 +15,100 @@ import datetime
 from tkinter import filedialog
 
 
-def read_csv_login(login,file):
+def read_csv(login:str,file:list,start:int,stop:int,status:int)->list:
     list_row=list()
     time_list=list()
+    first_row=0
     for file_from_directory in file:
-        i=0
-        #time_list=list()
-        start_row=''
+        start_row=""
         with open(file_from_directory) as f:
-           reader = csv.reader(f)
-           checks=0
-           for row in reader:
-               if checks>0 and row[3]==login:
-                   rows=str()
-                   check=0
-                   for j in row:
-                       if check==0:
-                           rows=rows+j
-                           check+=1
-                       else:
-                           rows=rows+","+j
-                   rows=rows+"\n"
-                   time_list.append(rows)
-               elif checks==0: 
-                   for j in row:
-                       if checks==0:
-                           start_row=start_row+j
-                           checks+=1
-                       else:
-                           start_row=start_row+","+j
-                   start_row=start_row+"\n"
-                   time_list.append(start_row)
-               else:pass
+            time_list=list()
+            reader = csv.reader(f)
+            for row in reader:
+                    if status==1:
+                        try:
+                            if first_row>0 and row[3]==login:
+                                rows=str()
+                                first_ellem=0
+                                for j in row:
+                                    if first_ellem==0:
+                                        rows=rows+j
+                                        first_ellem+=1
+                                    else:
+                                        rows=rows+","+j
+                                rows=rows+"\n"
+                                time_list.append(rows)
+                            elif first_row==0: 
+                                for j in row:
+                                    if first_row==0:
+                                        start_row=start_row+j
+                                        first_row+=1
+                                    else:
+                                        start_row=start_row+","+j
+                                start_row=start_row+"\n"
+                                time_list.append(start_row)
+                        except Exception:
+                            print("Error")
+                    elif status==2:
+                        try:
+                            if first_row>0 and int(row[0])>=int(stop) and int(row[1])<=int(start):
+                                print(row)
+                                rows=str()
+                                first_ellem=0
+                                for j in row:
+                                    if first_ellem==0:
+                                        rows=rows+str(j)
+                                        first_ellem+=1
+                                    else:
+                                        rows=rows+","+str(j)
+                                rows=rows+"\n"
+                                time_list.append(rows)
+                            elif first_row==0: 
+                                for j in row:
+                                    if first_row==0:
+                                        start_row=start_row+str(j)
+                                        first_row+=1
+                                    else:
+                                        start_row=start_row+","+str(j)
+                                start_row=start_row+"\n"
+                                time_list.append(start_row)
+                        except Exception:
+                            print("Error")
         list_row.append(time_list)
     p=list()
-    p.append(time_list)
+    p.append(list_row)
+    p1=list()
+    p1.append(p)
+    print("LOH",p)
     return p
 
 
-def read_csv(start,stop,file):
-    list_row=list()
-    time_list=list()
-    for file_from_directory in file:
-        i=0
-        #time_list=list()
-        start_row=''
-        with open(file_from_directory) as f:
-           reader = csv.reader(f)
-           checks=0
-           for row in reader:
-               if checks>0 and int(row[0])>=stop and int(row[1])<=start:
-                   rows=str()
-                   check=0
-                   for j in row:
-                       if check==0:
-                           rows=rows+j
-                           check+=1
-                       else:
-                           rows=rows+","+j
-                   rows=rows+"\n"
-                   time_list.append(rows)
-               elif checks==0: 
-                   for j in row:
-                       if check==0:
-                           start_row=start_row+j
-                           check+=1
-                       else:
-                           start_row=start_row+","+j
-                   start_row=start_row+"\n"
-                   time_list.append(start_row)
-               else:pass
-        list_row.append(time_list)
-    p=list()
-    p.append(time_list)
-    return p
+async def antiproblem(status,login,file,start,stop): 
+        print("antiproblem")
+        if status==1:
+            log=login
+            fil=file
+            star=0
+            sto=0
+        if status==2:
+            log=""
+            fil=file
+            star=int(start)
+            sto=int(stop)
+        with mp.Pool(processes=1) as my_pool:
+            p1=my_pool.starmap(read_csv,
+                                    iterable=[
+                                              [log,fil,star,sto,status]
+                                             ]
+                                    ) 
+        return p1
+        
 
 
 class GUI():
 
-
-    def antiproblem(self):  
-        login=self.login
-        file=self.file
-        with mp.Pool(processes=1) as my_pool:
-            self.p1=my_pool.starmap(read_csv_login,
-                                    iterable=[
-                                              [login,file]
-                                             ]
-                                    ) 
-
-
-    def antiproblem_2(self):  
-        start=int(self.start)
-        stop=int(self.stop)
-        file=self.file
-        with mp.Pool(processes=1) as my_pool:
-            self.p1=my_pool.starmap(read_csv,
-                                    iterable=[
-                                              [start,stop,file]
-                                             ]
-                                    ) 
-        
-
-    def read_csv_Time(self,):
-        self.list_row=list()
-        i=0
-        with open(self.file) as f:
-            reader = csv.reader(f)
-            for row in reader:
-                if self.Time_check==0:
-                    check=row[0]
-                    if check>row[0]:
-                        check=row[0]
-                elif self.Time_check!=0:
-                    pass
-                else:pass
-
+    def __init__(self):
+        self.my_pool=mp.Pool(1)
 
     def back_in_menu(self):
         lst=self.win.grid_slaves()
@@ -141,6 +120,7 @@ class GUI():
 
 
     def Start_Win(self):
+        self.list_row=list()
         self.win=Tk()
         self.win.title("Lab_6")
         self.win.geometry("1360x480")
@@ -229,25 +209,26 @@ class GUI():
 
     def check_param(self):
         Errors=0
+        time=0
+        func_start=0
         self.login=self.login_en.get()
         if len(self.login)<1:
-            messagebox.showinfo('Логин', 'обосрався')
+            messagebox.showinfo('Логин', 'incorrect')
             Errors+=1
         self.rows=int(self.combo.get())
         if self.rows==10 or self.rows==20 or self.rows==30:
-            pass
+            time+=1
         else: 
-            messagebox.showinfo('Rows', 'обосрався')
+            messagebox.showinfo('Rows', 'incorrect')
             Errors+=1
-        if  self.file!=None:
-            pass
+        if  self.file!=None and time==1:
+            self.my_pool.apply_async(func=read_csv,args=("",self.file,0,0,2),callback=self.table_data) 
+            func_start=1
         else: 
-            messagebox.showinfo('Way', 'обосрався')
+            messagebox.showinfo('Way', 'incorrect')
             Errors+=1
-        if Errors==0:
-            self.finish_click_1()
-
-
+        if Errors==0 and func_start==1:
+            self.finish_click(1)
 
             
     def check_param_2(self):
@@ -264,64 +245,57 @@ class GUI():
         self.hour_2_1=self.hour_2.get()
         self.min_2_1=self.min_2.get()
         self.sec_2_1=self.sec_2.get()
-        try:
-            if int(self.day_1)<31 and int(self.day_1)>0 and int(self.day_2_1)<31 and int(self.day_2_1)>0:
-                pass
-            else:
-                messagebox.showinfo('Days', 'обосрався')
-            if int(self.month_1)<13 and int(self.month_1)>0 and int(self.month_2_1)<13 and int(self.month_2_1)>0:
-                pass
-            else:
-                messagebox.showinfo('Month', 'обосрався')
-            if int(self.year_1)<3000 and int(self.year_1)>1969 and int(self.year_2_1)<3000 and int(self.year_2_1)>1969 and int(self.year_1)>=int(self.year_2_1):
-                pass
-            else:
-                messagebox.showinfo('Years', 'обосрався')
-            if int(self.hour_1)<25 and int(self.hour_1)>-1 and int(self.hour_2_1)<25 and int(self.hour_2_1)>-1:
-                pass
-            else:
-                messagebox.showinfo('Hours', 'обосрався')
-            if int(self.min_1)<60 and int(self.min_1)>-1 and int(self.min_2_1)<60 and int(self.min_2_1)>-1:
-                pass
-            else:
-                messagebox.showinfo('Mins', 'обосрався')
-            if int(self.sec_1)<60 and int(self.sec_1)>-1 and int(self.sec_2_1)<60 and int(self.sec_2_1)>-1:
-                pass
-            else:
-                messagebox.showinfo('Secs', 'обосрався')
-            self.rows=int(self.combo.get())
-            if self.rows==10 or self.rows==20 or self.rows==30:
-                pass
-            else: 
-                messagebox.showinfo('Rows', 'обосрався')
-                Errors+=1
-            if  self.file!=None:
-                pass
-            else: 
-                messagebox.showinfo('Way', 'обосрався')
-                Errors+=1
-        except Exception:
-            messagebox.showinfo('Problem with time', 'обосрався')
-            Errors+=1
-        if Errors==0:
-            print("lol")
-            #self.hour_1=self.hour_1+10
-            #self.hour_2_1=self.hour_2_1+10
-            #if self.hour_1%24!=0:
-            #    p=self.hour_1%24
-            #    self.hour_1=p
-            #    self.day_1+=1
-            #if self.hour_2_1%24!=0:
-            #    p=self.hour_2_1%24
-            #    self.hour_2_1=p
-            #    self.day_2_1+=1
+        time=0
+        func_start=0
+        #try:
+        if int(self.day_1)<31 and int(self.day_1)>0 and int(self.day_2_1)<31 and int(self.day_2_1)>0:
+            time+=1
+        else:
+            messagebox.showinfo('Days', 'incorrect')
+        if int(self.month_1)<13 and int(self.month_1)>0 and int(self.month_2_1)<13 and int(self.month_2_1)>0:
+            time+=1
+        else:
+            messagebox.showinfo('Month', 'incorrect')
+        if int(self.year_1)<3000 and int(self.year_1)>1969 and int(self.year_2_1)<3000 and int(self.year_2_1)>1969 and int(self.year_1)>=int(self.year_2_1):
+            time+=1
+        else:
+            messagebox.showinfo('Years', 'incorrect')
+        if int(self.hour_1)<25 and int(self.hour_1)>-1 and int(self.hour_2_1)<25 and int(self.hour_2_1)>-1:
+            time+=1
+        else:
+            messagebox.showinfo('Hours', 'incorrect')
+        if int(self.min_1)<60 and int(self.min_1)>-1 and int(self.min_2_1)<60 and int(self.min_2_1)>-1:
+            time+=1
+        else:
+            messagebox.showinfo('Mins', 'incorrect')
+        if int(self.sec_1)<60 and int(self.sec_1)>-1 and int(self.sec_2_1)<60 and int(self.sec_2_1)>-1:
+            time+=1
+        else:
+            messagebox.showinfo('Secs', 'incorrect')
+        self.rows=int(self.combo.get())
+        if time==6:
             self.start=datetime.datetime(int(self.year_1),int(self.month_1),int(self.day_1), int(self.hour_1),int(self.min_1),int(self.sec_1)).timestamp()
             self.stop=datetime.datetime(int(self.year_2_1),int(self.month_2_1),int(self.day_2_1), int(self.hour_2_1),int(self.min_2_1),int(self.sec_2_1)).timestamp()
+        if self.rows==10 or self.rows==20 or self.rows==30:
+            pass
+        else: 
+            messagebox.showinfo('Rows', 'incorrect')
+            Errors+=1
+        if  self.file!=None and time==6:
+            self.my_pool.apply_async(func=read_csv,args=("",self.file,self.start,self.stop,2),callback=self.table_data) 
+            func_start=1
+        else: 
+            messagebox.showinfo('Way', 'incorrect')
+            Errors=1         
+        #except Exception:
+        #    messagebox.showinfo('Problem with time', 'incorrect')
+        #    Errors+=1
+        if Errors==0 and func_start==1:
             if self.start<self.stop:
-                messagebox.showinfo('Data wrong - start<stop', 'обосрався')
+                messagebox.showinfo('Data wrong - start<stop', 'incorrect')
                 Errors+=1
             if Errors==0:
-                self.finish_click_2()
+                self.finish_click(2)
 
 
     def way_file(self):
@@ -332,97 +306,84 @@ class GUI():
         for i in file:
             j=i.split(".")
             if j[-1]=="csv":
-                self.file.append(i)
+                i_1=os.path.join(directory,i)
+                print(i_1)
+                self.file.append(i_1)
+        print(self.file)
         if len(file)<1:
-            messagebox.showinfo('Havent files', 'обосрався')
+            messagebox.showinfo('Havent files', 'Error')
         else: 
             self.lbl_3.configure(text="we have directory and see files")
-        #self.file = filedialog.askopenfilename(filetypes = (("TXT files", "*.txt"),
-        #                                              ("HTML files", "*.html;*.htm"),
-        #                                              ("All files", "*.*") ))
-        #self.file_test=self.file.split(".")
-        #flag=True
-        #print(self.file)
-        #while flag:
-        #    if self.file_test[-1]!="csv":
-        #            self.file = filedialog.askopenfilename(filetypes = (("TXT files", "*.txt"),
-        #                                                  ("HTML files", "*.html;*.htm"),
-        #                                                  ("All files", "*.*") ))
-        #    else: flag=False
-        #self.lbl_3.configure(text=self.file)
 
+    def table_data(self,p):
+        p1=p[0] 
+        self.list_row=p1[0]
+        self.list_check=1
+        print(p,"----------------------",p1[0])
 
-    def finish_click_2(self):
-        lst=self.win.grid_slaves()
-        for l in lst:
-            l.destroy()
-        self.antiproblem_2()
-        self.rowrows=0
-        self.num=1
-        p=self.p1[0] 
-        self.list_row=p[0]
-        ttk.Button(self.win, text='Вывести выбранное количество сессий',command=self.output).pack()
-        ttk.Separator(self.win, orient=HORIZONTAL).pack(fill=BOTH)  
-        self.console = scrolledtext.ScrolledText(self.win,width=1360, state="disabled")
-        self.console.pack()
-        back=Button(self.win, text="back in menu", name="back", command=self.back_in_menu)
-        back.pack()
-        self.save=True
-        if self.save!=False:
-            back=Button(self.win, text="save result", command=self.save_result)
-            back.pack()
 
     def output(self):
-        self.console.configure(state='normal')
-        text=''
-        for i in range(0,self.rows):
-            try:
-                text=text+str(self.num)+"|"+self.list_row[i+self.rowrows]
-                self.num+=1
-            except IndexError:
-                pass
-        self.rowrows+=self.rows
-        self.console.insert(END,text)
-        self.console.yview(END)
+        print(self.list_check)
+        if len(self.list_row)>0 and len(self.list_row)<self.rows:
+            self.console.configure(state='normal')
+            text=''
+            for i in range(0,self.rows):
+                try:
+                    text=text+str(self.num)+"|"+self.list_row[i+self.rowrows]
+                    self.num+=1
+                except IndexError:
+                    pass
+            self.rowrows+=self.rows
+            self.console.insert(END,text)
+            self.console.yview(END)
+        else:
+            self.console.configure(state='normal')
+            text=''
+            for i in range(0,self.rows):
+                try:
+                    text=text+str(self.num)+"|"+self.list_row[i+self.rowrows]
+                    self.num+=1
+                except IndexError:
+                    pass
+            self.rowrows+=self.rows
+            self.console.insert(END,text)
+            self.console.yview(END)
 
 
     def save_result(self):
         text=""
         check=0
-        for j in self.start_row:
-            if check==0:
-                text=text+j
-                check+=1
-            else:
-                text=text+","+j
         text=text+"\n"
         with open("result.csv","w") as file:
             for i in self.list_row:
                 text=text+i
             file.write(text)
-        self.save=False
+        lst=self.win.grid_slaves()
 
-    def finish_click_1(self):
+    def finish_click(self,status):
         lst=self.win.grid_slaves()
         for l in lst:
             l.destroy()
-        self.antiproblem()
+        #self.antiproblem(status)
         self.rowrows=0
         self.num=1
-        p=self.p1[0]
-        self.list_row=p[0]
+        #p=self.p1[0]
+        #self.list_row=p[0]
         ttk.Button(self.win, text='Вывести выбранное количество сессий',command=self.output).pack()
         ttk.Separator(self.win, orient=HORIZONTAL).pack(fill=BOTH)
         self.console = scrolledtext.ScrolledText(self.win,width=1360, state="disabled")
         self.console.pack()
         back=Button(self.win, text="back in menu", name="back", command=self.back_in_menu)
         back.pack()
-        self.save=True
-        if self.save!=False:
-            back=Button(self.win, text="save result", command=self.save_result)
-            back.pack()
+        back=Button(self.win, text="save result",name="save", command=self.save_result)
+        back.pack()
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     obj4=GUI()
     obj4.Start_Win()
-     
+
+
+
+
+
